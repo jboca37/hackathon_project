18dd09370dbdd259b776ac63416a507b4d28e18e
Jesus Bocanegra <jojadev@proton.me>
2025-03-15 19:32:19 -0500

    remove supabase since were not using it

--- src/lib/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://ojxnffcluhhzqupdhcrp.supabase.co.', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qeG5mZmNsdWhoenF1cGRoY3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwMjI2MzAsImV4cCI6MjA1NzU5ODYzMH0.JTMEwqkvFSN_o_MFQnS_c-ln6a3UzqJRrbeon34m4WI')
